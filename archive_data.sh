b2f3949f01ac48b000b55ff8c7524b2436533d6b
jrjhealey <jrj.healey@gmail.com>
2018-08-04 21:37:12 +0100

    expanded help and arg checks

--- archive_data.sh
+++ archive_data.sh
@@ -9,16 +9,34 @@ set -eo pipefail
 
 curdir=$(pwd)
 targetdir="$1"
-usage()
-{
+
+usage(){
+tr=$(tput setaf 1)
+df=$(tput sgr0)
 cat << EOF
-usage: $0 <data directory> [--dry-run]
+
+usage: $0 <data directory> [--dry-run] [--discard-img]
+
+The only mandatory argument is the directory of data, and
+this must be the first argument. This should be a directory
+of directories, e.g:
+
+∟⎼⎼ Data  < Pass this filepath
+  ∟⎼⎼ 01012000_M01757_0123_000000000-ABCDE/
 
 This script compresses data from our MiSeq platform.
 In future it may be generalised to support all formats.
 
 Optionally, the script can be dry run to avoid changing the
 files, instead just printing out what will be changed.
+
+              ${tr}[OFF by defaut]${df}
+
+Also optionally the script can purge unecessary and large
+image files (.jpg/.tif), since only the basecalls/fastqs are
+really needed.
+
+              ${tr}[ON by default]${df}
 
 EOF
 }
@@ -53,8 +71,12 @@ fi
 
 if [[ -z "$2" ]] ; then
  dryrun="False"
-else
- dryrun="True"
+ else
+ if [ "$2" != "--dry-run" ] ; then
+  usage
+  err "Did not recognise the argument to dry run, ensure spelling and argument order are correct. Exiting." ; exit 1
+ fi
+  dryrun="True"
 fi
 
 ################################################################
@@ -63,6 +85,7 @@ cd "$targetdir"
 log "Moved to ${targetdir}..."
 
 if [[ -z $(find ./ -maxdepth 1 -type d -name "*[0-9][0-9][0-1][0-9][0-9][0-9]_M01757*" -print -quit) ]] ; then
+ usage
  err "No suitably named directories found. Maybe everything is already compressed?. Exiting." ; exit 1
 fi
 
